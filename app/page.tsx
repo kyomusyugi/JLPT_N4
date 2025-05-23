627b2150ab2de122bdd5e34702f509bbfd3a580e
kyomusyugi <k0esij@naver.com>
2025-05-23 19:21:47 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import { useState } from "react";
 import { words_01_05 } from "@/lib/words_01_05";
 import { words_06_10 } from "@/lib/words_06_10";
 import { words_11_15 } from "@/lib/words_11_15";
+import { words_16_20 } from "@/lib/words_16_20";
 import QuizPage from "@/components/quizpage";
 
 const units = {
   "Unit 01~05": words_01_05,
   "Unit 06~10": words_06_10,
   "Unit 11~15": words_11_15,
+  "Unit 16~20": words_16_20,
 };
 
 export default function HomePage() {
